32ba8e0ac458d898fd7cf50de7b5589ca6d1c6e8
jodgey <jordon.ulrich@yahoo.com>
2019-01-25 15:35:38 -0700

    Update PurchaseOrder.java

--- src/main/java/ocp/s2i/springboot/rest/model/PurchaseOrder.java
+++ src/main/java/ocp/s2i/springboot/rest/model/PurchaseOrder.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2005-2016 Red Hat, Inc.
- *
+ * 
  * Red Hat licenses this file to you under the Apache License, version
  * 2.0 (the "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
